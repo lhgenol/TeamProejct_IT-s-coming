c5cfdca3c0d394dbd8cfa7657ac66e1b6344581b
judelover27 <judelover27@gmail.com>
2025-03-12 16:27:51 +0900

    feat folder정리

--- Assets/02.Scripts/Manager/PlayerManager.cs
+++ Assets/02.Scripts/Manager/PlayerManager.cs
@@ -1,8 +1,8 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundManager : MonoBehaviour
+public class PlayerManager : Singleton<PlayerManager>
 {
     // Start is called before the first frame update
     void Start()
