dcb737c565df02271fedd9eb7697b3d310de6fd5
dongseok-kim <kdsehdtjr12@gmail.com>
2025-03-14 11:24:29 +0900

    Feat:ItemUI , Feat : ConstomizinUI

--- Assets/02.Scripts/UI/MainMenuUI/CustomizingUI.cs
+++ Assets/02.Scripts/UI/MainMenuUI/CustomizingUI.cs
@@ -12,8 +12,36 @@ public class CustomizingUI : BaseUI
     {
         base.Init(uiManager);
     }
+    void Start()
+    {
+        SetUpButtonListeners();
+    }
     protected override UIState GetUIState()
     {
         return UIState.Customizing;
+    }
+    void SetUpButtonListeners()
+    {
+        for (int i = 0; i < hats.Length; i++)
+        {
+            int index = i + 1; 
+            hats[i].onClick.AddListener(() => OnButtonClick("Hat", index));
+        }
+
+        for (int i = 0; i < outWears.Length; i++)
+        {
+            int index = i + 1; 
+            outWears[i].onClick.AddListener(() => OnButtonClick("OutWears", index));
+        }
+
+        for (int i = 0; i < pants.Length; i++)
+        {
+            int index = i + 1;
+            pants[i].onClick.AddListener(() => OnButtonClick("Pant", index));
+        }
+    }
+    void OnButtonClick(string itemType, int index)
+    {
+        Debug.Log($"Selected Item: {itemType},{index}");
     }
 }
